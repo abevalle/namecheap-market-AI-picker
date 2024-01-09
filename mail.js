e8e4af1e79b756b51751a2a6a0032c48d9512f00
abe valle <abe@valle.us>
2024-01-09 00:56:25 -0600

    i forgot

--- mail.js
+++ mail.js
@@ -22,15 +22,9 @@ export const sendMail = async (reciever, dataUrl = "<p>Nothing Here</p>") => {
       text: "Hello world?", // plain text body
       html: "<b>Here is your daily auction list</b>"
       + "<br></br>"
-      + `<p><a href=\"${dataUrl}\">Domain List Download</a></p>`
+      + `<p><a href=\'${dataUrl}\'>Domain List Download</a></p>`
+      + `<p>${dataUrl}</p>`
     });
   
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    //
-    // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-    //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-    //       <https://github.com/forwardemail/preview-email>
-    //
   }
